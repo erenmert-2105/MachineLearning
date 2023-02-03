20c14d287ec9f8cc0f453cef561546a8ea296cc0
Mert Eren <105564577+erenmert-2105@users.noreply.github.com>
2023-02-03 23:43:34 +0300

    Update CartPole-v0.py

--- Deepqlearning/CartPole-v0.py
+++ Deepqlearning/CartPole-v0.py
@@ -1,3 +1,4 @@
+#%%
 import gym
 import numpy as np
 from collections import deque
@@ -6,54 +7,62 @@ from keras.layers import Dense
 from keras.optimizers import Adam
 import random
 from IPython.display import clear_output
+import tensorflow as tf
+import time
+import psutil
 
+# Configure TensorFlow to use GPU
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    tf.config.experimental.set_visible_devices(physical_devices[0], 'GPU')
+else:
+    print("No GPU found")
 
-
         
-class DQLAgent:
-    def __init__(self, env):
-        # parameter / hyperparameter
-        self.state_size = env.observation_space.shape[0]
-        self.action_size = env.action_space.n
-        #%%
-        """
-        self.gama: This is the discount factor (gamma), which determines the
-        importance of future rewards in the Q-Learning algorithm. It is a value 
-        between 0 and 1, with a higher value indicating that future rewards are 
-        more important.
-
-        self.leraning_rate: This is the learning rate, which determines the 
-        size of the update step taken at each iteration of the Q-Learning 
-        algorithm. A smaller learning rate may result in slower convergence, 
-        while a larger learning rate may cause overshooting or instability.
-        
-        self.epsilon: This is the exploration rate or epsilon,
-        which determines the probability of the agent taking a random 
-        action instead of following the action with the highest Q-value.
-        A higher value of epsilon means that the agent is more likely 
-        to explore its environment, while a lower value means it is more 
-        likely to exploit what it already knows.
-        
-        self.epsilon_decay: This is the rate at which the value of
-        epsilon is decreased over time. This allows the agent to gradually 
-        reduce its exploration and increase its exploitation as it gains more
-        information about the environment.
-
-        self.epsilon_min: This is the minimum value that epsilon can take.
-        This sets a lower bound on the exploration rate and ensures that the
-        agent will never fully stop exploring its environment.
-        """
-        #%%
-        self.gamma = 0.95
-        self.learning_rate = 0.001 
-        
-        self.epsilon = 1  # explore
-        self.epsilon_decay = 0.995
-        self.epsilon_min = 0.01
-        
-        self.memory = deque(maxlen = 1000)
-        
-        self.model = self.build_model()
+class DeepRf:
+    
+    def __init__(self,env,gamma,learning_rate,epsilon):
+       # parameter / hyperparameter
+       self.state_size = env.observation_space.shape[0]
+       self.action_size = env.action_space.n
+       #%%
+       """
+       self.gama: This is the discount factor (gamma), which determines the
+       importance of future rewards in the Q-Learning algorithm. It is a value 
+       between 0 and 1, with a higher value indicating that future rewards are 
+       more important.
+       self.leraning_rate: This is the learning rate, which determines the 
+       size of the update step taken at each iteration of the Q-Learning 
+       algorithm. A smaller learning rate may result in slower convergence, 
+       while a larger learning rate may cause overshooting or instability.
+       
+       self.epsilon: This is the exploration rate or epsilon,
+       which determines the probability of the agent taking a random 
+       action instead of following the action with the highest Q-value.
+       A higher value of epsilon means that the agent is more likely 
+       to explore its environment, while a lower value means it is more 
+       likely to exploit what it already knows.
+       
+       self.epsilon_decay: This is the rate at which the value of
+       epsilon is decreased over time. This allows the agent to gradually 
+       reduce its exploration and increase its exploitation as it gains more
+       information about the environment.
+       self.epsilon_min: This is the minimum value that epsilon can take.
+       This sets a lower bound on the exploration rate and ensures that the
+       agent will never fully stop exploring its environment.
+       """
+       #%%
+       self.gamma = gamma
+       self.learning_rate = learning_rate
+       
+       self.epsilon = epsilon  # explore
+       self.epsilon_decay = 0.995
+       self.epsilon_min = 0.01
+       
+       self.memory = deque(maxlen = 1000)
+       
+       self.model = self.build_model()
         
         
     def build_model(self):
@@ -64,7 +73,6 @@ class DQLAgent:
         However, the sigmoid activation function can cause the vanishing 
         gradient problem, where the gradients become very small, making
         it difficult to train deep networks.
-
         Tanh: The tanh activation function maps any input to a value
         between -1 and 1. It is similar to the sigmoid function, but 
         the outputs are centered around 0, which can help the model
@@ -124,16 +132,32 @@ class DQLAgent:
     def adaptiveEGreedy(self):
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+test=0
+save=0
+
+def get_ram_usage():
+    return psutil.virtual_memory().percent
 
 if __name__ == "__main__":
     
     # initialize gym env and agent
     env = gym.make("CartPole-v1")
-
-    agent = DQLAgent(env)
+#%%    
+    gamma = 0.95 
+    # reward importance
+    learning_rate = 0.001
+    # high learning rate results in quick but unstable convergence, while a low learning rate may produce slow but stable convergence
+    epsilon = 1 
+    # explore rate do not change that its changing over time in algo
+    batch_size = 32
+    # it is how many steps between backward and forward 
+    # it effect result dramaticly low batch_size makes more accure and high makes more experiance so learns faster but too high or low makes learning harder
     
-    batch_size = 16
-    episodes = 2
+    episodes = 20
+    # how many times play after done 
+    
+    agent = DeepRf(env,gamma,learning_rate,epsilon)
+# high batch size means more experiance and it effects dramaticly on the other hand when you train it with low batch size your system more stable
     for e in range(episodes):
         
         # initialize environment
@@ -142,71 +166,64 @@ if __name__ == "__main__":
         state = np.reshape(state,[1,4])
         
         time = 0
-        while True:
-            
-            # act
-            action = agent.act(state) # select an action
-            
-            # step
-            next_state, reward, done, _ = env.step(action)
-            next_state = np.reshape(next_state,[1,4])
-            
-            # remember / storage
-            agent.remember(state, action, reward, next_state, done)
-            
-            # update state
-            state = next_state
-            
-            # replay
-            agent.replay(batch_size)
-            
-            # adjust epsilon
-            agent.adaptiveEGreedy()
-            
-            time += 1
-            
-            if done:
-                print("Episode: {}, time: {}".format(e,time))
-                break
-            
-            
-# %% test
-import time
-trained_model = agent
-state = env.reset()
-state = np.reshape(state, [1,4])
-step = 0
-t_reward=0
-
-while True:
-    action = trained_model.act(state)
-    next_state, reward, done, _ = env.step(action)
-    t_reward=t_reward+reward
-    next_state = np.reshape(next_state, [1,4])
-    state = next_state
-    step += 1
-    
-    print("Step: {},Total Reward: {}".format(step,t_reward))
-    #time.sleep(0.4)
-    if done:
-        break
-print("Done")
-            
-            
-
-
-    
-
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-    
+        if get_ram_usage() >= 90:
+            agent.save("DRFmodel.h5")
+            break
+        else: 
+            while True:
+                
+                # act
+                action = agent.act(state) # select an action
+                
+                # step
+                next_state, reward, done, _ = env.step(action)
+                next_state = np.reshape(next_state,[1,4])
+                
+                # remember / storage
+                agent.remember(state, action, reward, next_state, done)
+                
+                # update state
+                state = next_state
+                
+                # replay
+                agent.replay(batch_size)
+                
+                # adjust epsilon
+                agent.adaptiveEGreedy()
+                
+                time += 1
+                if done:
+                    print("Episode: {}, time: {}".format(e,time))
+                    break
+                
+                 
+                
+     # %% test
+if test==1:   
+   
+   trained_model = agent
+   state = env.reset()
+   state = np.reshape(state, [1,4])
+   step = 0
+   t_reward=0
+   while True:
+       action = trained_model.act(state)
+       next_state, reward, done, _ = env.step(action)
+       t_reward=t_reward+reward
+       next_state = np.reshape(next_state, [1,4])
+       state = next_state
+       step += 1
+       
+       print("Step: {},Total Reward: {}".format(step,t_reward))
+       #time.sleep(0.4)
+       if done:
+           break
+   print("Done")
+                
+if save == 1:
+    agent.save("DRFmodel.h5")                               
+                    
+"""
+agent = tf.keras.models.load_model("DRFmodel.h5")
+"""                
+                
